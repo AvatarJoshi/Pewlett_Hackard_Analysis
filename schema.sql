b7359c5cbd2b66cc2a0dc1e4b0d1830762feef2b
Avatar Joshi <joshi.avatar@gmail.com>
2022-07-25 08:36:10 -0700

    Added extra queries for table JOINS

--- schema.sql
+++ schema.sql
@@ -36,14 +36,16 @@ PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE department_employees (
-dept_no VARCHAR NOT NULL,
 emp_no INT NOT NULL,
+dept_no VARCHAR NOT NULL,
 from_date DATE NOT NULL,
 to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (emp_no, dept_no)
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
+
+SELECT * FROM department_employees;
+DROP TABLE department_employees;
 
 CREATE TABLE titles (
 emp_no INT NOT NULL,
@@ -58,19 +60,54 @@ SELECT * FROM TableNameHere;
 DROP TABLE TableNameHere CASCADE;
 
 -- Employees retiring
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
 -- Check that the table was made
 SELECT * FROM retirement_info;
+
+-- Drop the retirement_info table
+DROP TABLE retirement_info;
+
+
 
 -- Count the number of employees retiring
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Join tables together
+SELECT first_name, last_name, dept_no
+FROM employees as e
+LEFT JOIN department_employees as t ON e.emp_no = t.emp_no
+
+-- Join departments and deptartment_manager tables
+SELECT d.dept_name,
+dm.emp_no,
+dm.from_date,
+dm.to_date
+FROM departments as d
+INNER JOIN department_managers as dm
+ON d.dept_no = dm.dept_no;
+
+
+-- Join retirement_info and department_employees tables
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+de.to_date
+FROM retirement_info as ri
+LEFT JOIN department_employees as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT * FROM retirement_info;
+SELECT * FROM department_employees;
+
+
+
 
 
